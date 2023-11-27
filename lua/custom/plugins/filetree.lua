15dad1ec6e1c5cd63e8e34895cb699593dd43d9d
Muhammed Hussein Karimi <info@karimi.dev>
2023-11-27 11:38:13 +0330

    ✨ feat: neo-tree show hidden files in a diffrent way, markdown-preview added
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -6,10 +6,16 @@ return {
   version = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
-    require('neo-tree').setup {}
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    }
   end,
 }
