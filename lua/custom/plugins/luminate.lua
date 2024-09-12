8eda130bff5bb8a25656cdc75acd2bc1ae3ec9d0
Muhammed Hussein Karimi <info@karimi.dev>
2024-09-12 23:08:55 +0330

    ✨ feat: luminate added
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

---
+++ lua/custom/plugins/luminate.lua
@@ -0,0 +1,9 @@
+return {
+  'mei28/luminate.nvim',
+  event = { 'VeryLazy' },
+  config = function()
+    require('luminate').setup {
+      -- if you want to customize, see Usage!
+    }
+  end,
+}
