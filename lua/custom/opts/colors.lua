20fa31ac90a31dd6632282280836c7a4211fb469
Muhammed Hussein Karimi <info@karimi.dev>
2024-12-04 22:45:11 +0330

    :lipstick: Bring back rainbow delimiters :)
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- lua/custom/opts/colors.lua
+++ lua/custom/opts/colors.lua
@@ -1,6 +1,6 @@
 local lualine = require 'lualine'
 local treesitter_configs = require 'nvim-treesitter.configs'
--- local rainbow_delimiters = require 'rainbow-delimiters'
+local rainbow_delimiters = require 'rainbow-delimiters'
 local catppuccin = require 'catppuccin'
 local sign = vim.fn.sign_define
 
@@ -81,49 +81,49 @@ function _M.setup()
   sign('DapLogPoint', { text = '◆', texthl = 'DapLogPoint', linehl = '', numhl = '' })
   vim.cmd 'colorscheme catppuccin'
   vim.o.background = 'dark'
-  -- vim.g.rainbow_delimiters = {
-  --   strategy = {
-  --     [''] = rainbow_delimiters.strategy['global'],
-  --     vim = rainbow_delimiters.strategy['local'],
-  --   },
-  --   query = {
-  --     [''] = 'rainbow-delimiters',
-  --     lua = 'rainbow-blocks',
-  --   },
-  --   highlight = {
-  --     'RainbowDelimiterRed',
-  --     'RainbowDelimiterYellow',
-  --     'RainbowDelimiterBlue',
-  --     'RainbowDelimiterOrange',
-  --     'RainbowDelimiterGreen',
-  --     'RainbowDelimiterViolet',
-  --     'RainbowDelimiterCyan',
-  --   },
-  -- }
-  -- local highlight = {
-  --   'RainbowRed',
-  --   'RainbowYellow',
-  --   'RainbowBlue',
-  --   'RainbowOrange',
-  --   'RainbowGreen',
-  --   'RainbowViolet',
-  --   'RainbowCyan',
-  -- }
+  vim.g.rainbow_delimiters = {
+    strategy = {
+      [''] = rainbow_delimiters.strategy['global'],
+      vim = rainbow_delimiters.strategy['local'],
+    },
+    query = {
+      [''] = 'rainbow-delimiters',
+      lua = 'rainbow-blocks',
+    },
+    highlight = {
+      'RainbowDelimiterRed',
+      'RainbowDelimiterYellow',
+      'RainbowDelimiterBlue',
+      'RainbowDelimiterOrange',
+      'RainbowDelimiterGreen',
+      'RainbowDelimiterViolet',
+      'RainbowDelimiterCyan',
+    },
+  }
+  local highlight = {
+    'RainbowRed',
+    'RainbowYellow',
+    'RainbowBlue',
+    'RainbowOrange',
+    'RainbowGreen',
+    'RainbowViolet',
+    'RainbowCyan',
+  }
   local hooks = require 'ibl.hooks'
   -- create the highlight groups in the highlight setup hook, so they are reset
   -- every time the colorscheme changes
   hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    -- vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
-    -- vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
-    -- vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
-    -- vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
-    -- vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
-    -- vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
-    -- vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+    vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
+    vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
+    vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+    vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
+    vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
+    vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
+    vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
   end)
 
-  -- vim.g.rainbow_delimiters = { highlight = highlight }
-  -- require('ibl').setup { scope = { highlight = highlight } }
+  vim.g.rainbow_delimiters = { highlight = highlight }
+  require('ibl').setup { scope = { highlight = highlight } }
 
   hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
   local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
@@ -159,12 +159,12 @@ function _M.setup()
         'bash',
       },
 
-      -- rainbow = {
-      --   enable = true,
-      --   disable = { 'jsx', 'cpp' },
-      --   query = 'rainbow-parens',
-      --   strategy = require('rainbow-delimiters').strategy.global,
-      -- },
+      rainbow = {
+        enable = true,
+        disable = { 'jsx', 'cpp' },
+        query = 'rainbow-parens',
+        strategy = require('rainbow-delimiters').strategy.global,
+      },
       sync_install = false,
       ignore_install = {},
       modules = {},
