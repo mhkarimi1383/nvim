6e21bbe5f963e05c2f2b3d7cdbe09e8b4ee77f4e
Muhammed Hussein Karimi <info@karimi.dev>
2024-12-06 18:02:50 +0330

    :sparkles: Image review
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

---
+++ lua/custom/plugins/image.lua
@@ -0,0 +1,34 @@
+return {
+  "3rd/image.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+    },
+  },
+  opts = {
+    backend = "kitty",
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = false,
+        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+      },
+      neorg = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = false,
+        filetypes = { "norg" },
+      },
+    },
+    max_width = nil,
+    max_height = nil,
+    max_width_window_percentage = nil,
+    max_height_window_percentage = 50,
+    kitty_method = "normal",
+  },
+}
