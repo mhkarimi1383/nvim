b78ad1ab1fc35ce748623929890f550e274f4704
Muhammed Hussein Karimi <info@karimi.dev>
2025-07-08 12:13:23 +0330

    :heavy_plus_sign: Add new netrw
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- lua/custom/opts/generic.lua
+++ lua/custom/opts/generic.lua
@@ -44,6 +44,23 @@ function _M.setup()
       kinds = { "comment", "imports" }, ---@type lsp.FoldingRangeKind[]
     },
   }
+  require("netrw").setup({
+    -- File icons to use when `use_devicons` is false or if
+    -- no icon is found for the given file type.
+    icons = {
+      symlink = '',
+      directory = '',
+      file = '',
+    },
+    -- Uses mini.icon or nvim-web-devicons if true, otherwise use the file icon specified above
+    use_devicons = true,
+    mappings = {
+      -- Function mappings receive an object describing the node under the cursor
+      ['p'] = function(payload) print(vim.inspect(payload)) end,
+      -- String mappings are executed as vim commands
+      ['<Leader>p'] = ":echo 'hello world'<CR>",
+    },
+  })
 end
 
 return _M
