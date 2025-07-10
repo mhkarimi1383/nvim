612b0b016a8b2229d8f02213a2bc5b75b4bb1b9c
Muhammed Hussein Karimi <info@karimi.dev>
2025-07-10 13:49:58 +0330

    :construction: Trying to fix wildler
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- lua/custom/plugins/wilder.lua
+++ lua/custom/plugins/wilder.lua
@@ -1,3 +1,19 @@
 return {
-  'gelguy/wilder.nvim',
+  "gelguy/wilder.nvim",
+  config = function()
+    local wilder = require("wilder")
+    wilder.setup({ modes = { ":", "/", "?" } })
+
+    wilder.set_option(
+      "renderer",
+      wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+        highlights = {
+          border = "Normal", -- highlight to use for the border
+        },
+        -- 'single', 'double', 'rounded' or 'solid'
+        -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+        border = "rounded",
+      }))
+    )
+  end,
 }
