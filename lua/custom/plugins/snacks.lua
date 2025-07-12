e2f1d4cb63c9bdb2d013b7bdffd9e3e03e174f54
Muhammed Hussein Karimi <info@karimi.dev>
2025-07-12 12:52:06 +0330

    :heavy_plus_sign: Add snacks.nvim
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    notifier = {
+      -- your notifier configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
+}
